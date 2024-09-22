f7a2a836e2d19a47c9615d523a3f6e9b56b03366
paniicj0 <130447934+paniicj0@users.noreply.github.com>
2024-09-22 20:04:24 +0200

    added

---
+++ LangLang/CLI/RepositorySQL/DataBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace LangLang.RepositorySQL
+{
+    public class DataBase
+    {
+        public static string ConnectionString { get; private set; }
+        private OracleConnection Connection { get; set; }
+
+        private static DataBase instance = null;
+
+        private DataBase()
+        {
+            ConnectionString = "\"Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.56.1)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=XE)));User Id=jovana;Password=ftn;\"\r\n";
+
+            Connection = new OracleConnection(ConnectionString);
+        }
+
+        public static DataBase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataBase();
+                }
+                return instance;
+            }
+        }
+
+        public OracleConnection GetConnection()
+        {
+            return Connection;
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                Connection.Open();
+                Console.WriteLine("Successfully connected to Oracle.");
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error connecting to Oracle: {e.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ LangLang/CLI/RepositorySQL/TutorRepositorySQL.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Oracle.ManagedDataAccess.Client;
+using LangLang.Model;
+using LangLang.ModelEnum;
+
+namespace LangLang.RepositorySQL
+{
+    public class TutorRepositorySQL
+    {
+        private readonly string connectionString = DataBase.ConnectionString;
+        private List<Tutor> tutors = new List<Tutor>();
+        private static TutorRepositorySQL instance = null;
+
+        public TutorRepositorySQL()
+        {
+            tutors = LoadData();
+        }
+
+        public static TutorRepositorySQL GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new TutorRepositorySQL();
+            }
+            return instance;
+        }
+        public List<Tutor> GetAll()
+        {
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("SELECT * FROM tutors", conn))
+                {
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            tutors.Add(new Tutor(
+                                id: reader.GetInt32(0),
+                                name: reader.GetString(1),
+                                surname: reader.GetString(2),
+                                gender: (GenderEnum.Gender)Enum.Parse(typeof(GenderEnum.Gender), reader.GetString(3)),
+                                birthday: reader.GetDateTime(4),
+                                phoneNumber: reader.GetInt32(5),
+                                email: reader.GetString(6),
+                                password: reader.GetString(7),
+                                languages: (LanguageEnum.Language)Enum.Parse(typeof(LanguageEnum.Language), reader.GetString(8)),
+                                languageLevel: (LanguageLevelEnum.LanguageLevel)Enum.Parse(typeof(LanguageLevelEnum.LanguageLevel), reader.GetString(9)),
+                                tutorCreationDate: reader.GetDateTime(10)
+                            ));
+                        }
+                    }
+                }
+            }
+            return tutors;
+        }
+
+        public Tutor GetById(int id)
+        {
+            Tutor tutor = null;
+
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("SELECT * FROM tutors WHERE id = :id", conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("id", id));
+
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            tutor = new Tutor(
+                                id: reader.GetInt32(0),
+                                name: reader.GetString(1),
+                                surname: reader.GetString(2),
+                                gender: (GenderEnum.Gender)Enum.Parse(typeof(GenderEnum.Gender), reader.GetString(3)),
+                                birthday: reader.GetDateTime(4),
+                                phoneNumber: reader.GetInt32(5),
+                                email: reader.GetString(6),
+                                password: reader.GetString(7),
+                                languages: (LanguageEnum.Language)Enum.Parse(typeof(LanguageEnum.Language), reader.GetString(8)),
+                                languageLevel: (LanguageLevelEnum.LanguageLevel)Enum.Parse(typeof(LanguageLevelEnum.LanguageLevel), reader.GetString(9)),
+                                tutorCreationDate: reader.GetDateTime(10)
+                            );
+                        }
+                    }
+                }
+            }
+            return tutor;
+        }
+
+        private int GenerateId()
+        {
+            int maxId = 0;
+
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("SELECT MAX(id) FROM tutors", conn))
+                {
+                    maxId = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            return maxId + 1;
+        }
+
+        public void Save(Tutor tutor)
+        {
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("INSERT INTO tutors (id, name, surname, gender, birthday, phoneNumber, email, password, languages, languageLevel, tutorCreationDate) VALUES (:id, :name, :surname, :gender, :birthday, :phoneNumber, :email, :password, :languages, :languageLevel, :tutorCreationDate)", conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("id", tutor.Id));
+                    cmd.Parameters.Add(new OracleParameter("name", tutor.Name));
+                    cmd.Parameters.Add(new OracleParameter("surname", tutor.Surname));
+                    cmd.Parameters.Add(new OracleParameter("gender", tutor.Gender.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("birthday", tutor.Birthday));
+                    cmd.Parameters.Add(new OracleParameter("phoneNumber", tutor.PhoneNumber));
+                    cmd.Parameters.Add(new OracleParameter("email", tutor.Email));
+                    cmd.Parameters.Add(new OracleParameter("password", tutor.Password));
+                    cmd.Parameters.Add(new OracleParameter("languages", tutor.Languages.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("languageLevel", tutor.LanguageLevel.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("tutorCreationDate", tutor.TutorCreationDate));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("DELETE FROM tutors WHERE id = :id", conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("id", id));
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Tutor tutor)
+        {
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("UPDATE tutors SET name = :name, surname = :surname, gender = :gender, birthday = :birthday, phoneNumber = :phoneNumber, email = :email, password = :password, languages = :languages, languageLevel = :languageLevel, tutorCreationDate = :tutorCreationDate WHERE id = :id", conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("id", tutor.Id));
+                    cmd.Parameters.Add(new OracleParameter("name", tutor.Name));
+                    cmd.Parameters.Add(new OracleParameter("surname", tutor.Surname));
+                    cmd.Parameters.Add(new OracleParameter("gender", tutor.Gender.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("birthday", tutor.Birthday));
+                    cmd.Parameters.Add(new OracleParameter("phoneNumber", tutor.PhoneNumber));
+                    cmd.Parameters.Add(new OracleParameter("email", tutor.Email));
+                    cmd.Parameters.Add(new OracleParameter("password", tutor.Password));
+                    cmd.Parameters.Add(new OracleParameter("languages", tutor.Languages.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("languageLevel", tutor.LanguageLevel.ToString()));
+                    cmd.Parameters.Add(new OracleParameter("tutorCreationDate", tutor.TutorCreationDate));
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Tutor Create(Tutor tutor)
+        {
+            if (GetByEmail(tutor.Email) != null)
+            {
+                return null;
+            }
+            tutor.Id = GenerateId();
+            Save(tutor);
+            return tutor;
+        }
+
+        private Tutor GetByEmail(string email)
+        {
+            Tutor tutor = null;
+
+            using (OracleConnection conn = new OracleConnection(connectionString))
+            {
+                conn.Open();
+                using (OracleCommand cmd = new OracleCommand("SELECT * FROM tutors WHERE email = :email", conn))
+                {
+                    cmd.Parameters.Add(new OracleParameter("email", email));
+
+                    using (OracleDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            tutor = new Tutor(
+                                id: reader.GetInt32(0),
+                                name: reader.GetString(1),
+                                surname: reader.GetString(2),
+                                gender: (GenderEnum.Gender)Enum.Parse(typeof(GenderEnum.Gender), reader.GetString(3)),
+                                birthday: reader.GetDateTime(4),
+                                phoneNumber: reader.GetInt32(5),
+                                email: reader.GetString(6),
+                                password: reader.GetString(7),
+                                languages: (LanguageEnum.Language)Enum.Parse(typeof(LanguageEnum.Language), reader.GetString(8)),
+                                languageLevel: (LanguageLevelEnum.LanguageLevel)Enum.Parse(typeof(LanguageLevelEnum.LanguageLevel), reader.GetString(9)),
+                                tutorCreationDate: reader.GetDateTime(10)
+                            );
+                        }
+                    }
+                }
+            }
+            return tutor;
+        }
+
+        public List<Tutor> LoadData()
+        {
+            List<Tutor> tutors = new List<Tutor>();
+
+            try
+            {
+                using (OracleConnection conn = new OracleConnection(connectionString))
+                {
+                    conn.Open();
+                    using (OracleCommand cmd = new OracleCommand("SELECT * FROM tutors", conn))
+                    {
+                        using (OracleDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                tutors.Add(new Tutor(
+                                    id: reader.GetInt32(0),
+                                    name: reader.GetString(1),
+                                    surname: reader.GetString(2),
+                                    gender: (GenderEnum.Gender)Enum.Parse(typeof(GenderEnum.Gender), reader.GetString(3)),
+                                    birthday: reader.GetDateTime(4),
+                                    phoneNumber: reader.GetInt32(5),
+                                    email: reader.GetString(6),
+                                    password: reader.GetString(7),
+                                    languages: (LanguageEnum.Language)Enum.Parse(typeof(LanguageEnum.Language), reader.GetString(8)),
+                                    languageLevel: (LanguageLevelEnum.LanguageLevel)Enum.Parse(typeof(LanguageLevelEnum.LanguageLevel), reader.GetString(9)),
+                                    tutorCreationDate: reader.GetDateTime(10)
+                                ));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Add logging or error handling here
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return tutors;
+        }
+
+    }
+
+}
