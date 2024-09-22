7abd05629adbd7ac3fad6b261b25e25c497bc3ef
paniicj0 <130447934+paniicj0@users.noreply.github.com>
2024-09-22 20:07:08 +0200

    rest

---
+++ LangLang/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using LangLang.CLI;
+using LangLang.Controllers;
+using LangLang.RepositorySQL;
+using LangLang.View;
+using System.Windows;
+using LangLang.RepositorySQL;
+namespace LangLang
+{
+    public partial class App : Application
+    {
+        public Login loginCLI = new Login();
+        public LogInWindow logInWindow = new LogInWindow();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            logInWindow.Show();
+            //CLIStartup();
+
+        }
+
+        // startup for wpf application
+        private void WPFStartup()
+        {
+            LogInWindow logInWindow = new LogInWindow();
+           // tutor.LoadFromDatabase();
+        }
+
+        // startup for cli application
+        private void CLIStartup()
+        {
+            loginCLI.Startup();
+        }
+    }
+}
