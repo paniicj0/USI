1feb5664543ca6bc2ce355596310000148ac15a9
paniicj0 <130447934+paniicj0@users.noreply.github.com>
2024-09-22 20:02:58 +0200

    added

---
+++ LangLang/CLI/Functionalities/Tutor/CreateCourseTutor.cs
@@ -0,0 +1,158 @@
+﻿using LangLang.CLI.Functionalities.Director;
+using LangLang.Controllers;
+using LangLang.Model;
+using LangLang.ModelEnum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.CLI.Functionalities.Tutor
+{
+    public class CreateCourseTutor
+    {
+        CourseController courseController;
+
+        public CreateCourseTutor()
+        {
+            courseController = new CourseController();
+        }
+
+        public void Run(int tutorId)
+        {
+            Console.WriteLine("Create new tutor: ");
+
+            LanguageEnum.Language language = GetLanguage();
+            LanguageLevelEnum.LanguageLevel languageLevel = GetLanguageLevel();
+            int duration = GetDuration();
+            DateTime start = GetStartDate();
+            List<DaysEnum.Days> days = GetDays();
+            RealizationEnum.Realization realization = GetRealization();
+            int students = GetMaxNumberOfStudents(realization);
+
+            Course course = new Course(-1, language, languageLevel, duration, days, start, realization, students, tutorId, 0);
+            courseController.Create(course);
+
+            Console.WriteLine("Course created successfully!");
+        }
+
+        private LanguageEnum.Language GetLanguage()
+        {
+            Console.WriteLine("Select Language:");
+            foreach (LanguageEnum.Language language in Enum.GetValues(typeof(LanguageEnum.Language)))
+            {
+                Console.WriteLine($"{(int)language} - {language}");
+            }
+            int choice = GetIntegerInput("Enter your choice:");
+            return (LanguageEnum.Language)choice;
+        }
+
+        private LanguageLevelEnum.LanguageLevel GetLanguageLevel()
+        {
+            Console.WriteLine("Select Language Level:");
+            foreach (LanguageLevelEnum.LanguageLevel level in Enum.GetValues(typeof(LanguageLevelEnum.LanguageLevel)))
+            {
+                Console.WriteLine($"{(int)level} - {level}");
+            }
+            int choice = GetIntegerInput("Enter your choice:");
+            return (LanguageLevelEnum.LanguageLevel)choice;
+        }
+
+        private int GetDuration()
+        {
+            return GetIntegerInput("Enter Duration (in weeks):");
+        }
+
+        private DateTime GetStartDate()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter Start Date (yyyy-mm-dd):");
+                string input = Console.ReadLine();
+                if (!DateTime.TryParse(input, out DateTime result))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid date in yyyy-mm-dd format.");
+                }
+                if (result > DateTime.Today)
+                {
+                    return result;
+                }
+                return result;
+            }
+        }
+
+        private List<DaysEnum.Days> GetDays()
+        {
+            List<DaysEnum.Days> days = new List<DaysEnum.Days>();
+
+            while (true)
+            {
+                Console.WriteLine("Select Days (separate by comma, e.g., 1,3,5):");
+                foreach (DaysEnum.Days day in Enum.GetValues(typeof(DaysEnum.Days)))
+                {
+                    Console.WriteLine($"{(int)day} - {day}");
+                }
+                string input = Console.ReadLine();
+                string[] choices = input.Split(',');
+
+                days.Clear();
+
+                bool allValid = true;
+
+                foreach (string choice in choices)
+                {
+                    if (!(int.TryParse(choice, out int dayInt) && Enum.IsDefined(typeof(DaysEnum.Days), dayInt)))
+                    {
+                        Console.WriteLine($"Invalid day option: {choice}. Please enter valid day options.");
+                        allValid = false;
+                        break;
+                    }
+                    days.Add((DaysEnum.Days)dayInt);
+                }
+
+                if (allValid)
+                {
+                    break;
+                }
+            }
+
+            return days;
+        }
+
+        private RealizationEnum.Realization GetRealization()
+        {
+            Console.WriteLine("Select Realization:");
+            foreach (RealizationEnum.Realization realization in Enum.GetValues(typeof(RealizationEnum.Realization)))
+            {
+                Console.WriteLine($"{(int)realization} - {realization}");
+            }
+            int choice = GetIntegerInput("Enter your choice:");
+            return (RealizationEnum.Realization)choice;
+        }
+
+        private int GetMaxNumberOfStudents(RealizationEnum.Realization realization)
+        {
+            if (realization == RealizationEnum.Realization.live)
+            {
+                Console.WriteLine("Enter Max Number of Students:");
+                return GetIntegerInput("Enter your choice:");
+            }
+            return 0;
+        }
+
+        private int GetIntegerInput(string prompt)
+        {
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out int result))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ LangLang/CLI/Functionalities/Tutor/DisplayExams.cs
@@ -0,0 +1,127 @@
+﻿using LangLang.Controllers;
+using LangLang.Model;
+using LangLang.WPF.RepositorySQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangLang.CLI.Functionalities.Tutor
+{
+    public class DisplayExams
+    {
+        private ExamController examController;
+        private TutorController tutorController;
+        private CreateExamTutor createExam;
+        private UpdateExam updateExam;
+        private DeleteExam deleteExam;
+
+        public DisplayExams()
+        {
+            examController = new ExamController();
+            tutorController = new TutorController();
+            createExam = new CreateExamTutor();
+            updateExam = new UpdateExam();
+            deleteExam = new DeleteExam();
+        }
+
+        public void Run(int tutorId)
+        {
+            while (true)
+            {
+                DisplayExamsTable(tutorId);
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Create exam");
+                Console.WriteLine("2. Update exam");
+                Console.WriteLine("3. Delete exam");
+                Console.WriteLine("4. Exit");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CreateExam(tutorId);
+                        break;
+                    case "2":
+                        UpdateExam();
+                        break;
+                    case "3":
+                        DeleteExam();
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice, please try again.");
+                        break;
+                }
+            }
+        }
+
+        private void DisplayExamsTable(int tutorId)
+        {
+            
+            List<Exam> exams = examController.GetAll(); 
+            if (exams.Count == 0)
+            {
+                Console.WriteLine("The list of exams is empty.");
+                return;
+            }
+            Console.WriteLine("Exams:");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("ID    | Language  | Language Level | Num of Students | Exam Date   | Exam Time  | Duration | Applied | Confirmed |");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
+
+            foreach (Exam exam in exams)
+            {
+                if (exam.TutorId == tutorId)
+                {
+                    string confirmedString = exam.Confirmed ? "Yes" : "No";
+                    Console.WriteLine($"{exam.Id,-5} | {exam.Language,-9} | {exam.LanguageLevel,-14} | {exam.NumOfStudents,-15} | {exam.ExamDate:yyyy-MM-dd} | {exam.ExamTime:hh\\:mm\\:ss} | {exam.ExamDuration,-8} | {exam.NumberOfAppliedStudents,-7} | {confirmedString,-9} |");
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------------");
+                }
+            }
+        }
+
+        private void CreateExam(int tutorId)
+        {
+            createExam.Run(tutorId);
+        }
+
+        private void UpdateExam()
+        {
+            Console.Write("Enter the ID of the exam to update: ");
+            if (int.TryParse(Console.ReadLine(), out int examId))
+            {
+                Exam selectedExam = examController.GetById(examId);
+                if (selectedExam != null)
+                {
+                    updateExam.Run(selectedExam.TutorId, selectedExam.Id);
+                }
+                else
+                {
+                    Console.WriteLine("Exam not found.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID.");
+            }
+        }
+
+        private void DeleteExam()
+        {
+            Console.Write("Enter the ID of the exam to delete: ");
+            if (int.TryParse(Console.ReadLine(), out int examId))
+            {
+                deleteExam.Run(examId);
+            }
+            else
+            {
+                Console.WriteLine("Invalid ID.");
+            }
+        }
+
+
+    }
+}
